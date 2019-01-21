0fde16c262311e048d0eca0d6e18480507a7c87f
Paulo <pevilhena2@gmail.com>
2019-01-21 09:31:22 +0100

    Complete task 5

--- tiny_mlp.j
+++ tiny_mlp.j
@@ -11,12 +11,20 @@
 				       1 + e^{-x}
 
 *)
+
+let (sigmoid : float -> float) =
+  fun (x : float) -> inv (1 + exp (neg x))
+
 let nn (p :
        (* inputs  *) (float * float) *
        (* weights *) (float * float))
        (* output  *) : float
 =
-   0.
+  let   (x : float) = fst (fst p) in
+  let   (y : float) = snd (fst p) in
+  let (wxu : float) = fst (snd p) in
+  let (wyu : float) = snd (snd p) in
+  sigmoid ((x * wxu) + (y * wyu))
 
 (*
 
--- tiny_mlp_test.ml
+++ tiny_mlp_test.ml
@@ -17,19 +17,52 @@ type training_set = ((float * float) * float) list
 (** A trained net must minimize the error function defined in tiny_mlp.j. *)
 let acceptable_error = 0.01
 
-let eval_net net training_set =
-  List.map (fun (input, expectation) ->
-      Eval.error ((input, net), expectation)
-    ) training_set
-  |> List.fold_left max min_float
+let error net (input, target) =
+  Eval.error ((input, net), target)
 
+let eval_net net training_set =
+  training_set |> List.map (error net) |> List.fold_left max min_float
+
+let cost net training_set =
+  let len = float_of_int (List.length training_set) in
+  training_set |> List.map (error net) |> List.fold_left (+.) 0. |> fun a -> a /. len
+
+let error_gradient net (input, target) =
+  let df x y =
+    DiffCat.epsilon_dapply Diff.error
+      ((input, net), target)
+      (((0., 0.), (x, y)), 0.) in
+  df 1. 0., df 0. 1.
+
+let cost_gradient net training_set =
+  let len = float_of_int (List.length training_set) in
+  let gradient_list = List.map (error_gradient net) training_set in
+  let aux f =
+    gradient_list |> List.map f |> List.fold_left (+.) 0. |> fun a -> a /. len in
+  aux fst, aux snd
+
+let gradient_descent ?(verbose=false) ~n_iterations ~step net training_set =
+  let rec aux n net =
+    begin if verbose && n mod 100 = 0 then
+      let cost = cost net training_set in
+      Printf.printf "%6d: (%2.5f, %2.5f) %f\n" n (fst net) (snd net) cost
+    end;
+    if n >= n_iterations then net else
+      let dwx, dwy = cost_gradient net training_set in
+      aux (n + 1) (update_net net (-.step *. dwx) (-.step *. dwy))
+  in
+  aux 0 net
 
 (** [train training_set] returns a neural network trained for the
     [training_set]. *)
-let train : training_set -> net = fun training_set ->
-   failwith "Student! This is your job!"
+let train : training_set -> net =
+  let rand a b = Random.init 13; (Random.float (b -. a)) +. a in
+  let rand_net a b = make_net (rand a b) (rand a b) in
+  gradient_descent ~n_iterations:10000 ~step:0.01 (rand_net (-.1.) 1.)
 
 let test =
   let training_set = [ (0., 1.), 0.; (1., 0.), 1. ] in
   let trained_net = train training_set in
+  let error = eval_net trained_net training_set in
+  Printf.printf "eval_error trained_net training_set = %f \n" error;
   assert (eval_net trained_net training_set < acceptable_error)
